1c480b9d2ead1315a67f16275980aba39964eb1d
Akaaljot Singh <akaaljotsingh@gmail.com>
2026-02-12 14:57:27 +0530

    add bill fixes

--- src/app/components/admin/bills/add-bills.component.ts
+++ src/app/components/admin/bills/add-bills.component.ts
@@ -49,7 +49,7 @@ export class AddBillsComponent implements OnInit {
     ),
     billAmount: new FormControl('', Validators.required),
     dueDate: new FormControl(''),
-    billType: new FormControl(''),
+    billType: new FormControl(1),
     status: new FormControl(0),
     image: new FormControl(''),
     comments: new FormControl(''),
